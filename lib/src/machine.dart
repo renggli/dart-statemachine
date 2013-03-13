4c328480fa41842ddc02c68dfa660f72195e68e0
Lukas Renggli <renggli@gmail.com>
2013-03-13 16:03:58 +0100

    - move library documentation to library itself

--- lib/src/machine.dart
+++ lib/src/machine.dart
@@ -3,42 +3,7 @@
 part of statemachine;
 
 /**
- * A state machine.
- *
- * To create a new state machine instantiate this class:
- *
- *     var machine = new Machine();
- *
- * To create states call [Machine#newState()] and store them in variables:
- *
- *     var inactive = machine.newState();
- *     var active = machine.newState();
- *
- * To reset a state machine to its initial state call [Machine#reset()]:
- *
- *     machine.reset();
- *
- * To enter a specific state call [State#enter()].
- *
- *     inactive.enter();
- *
- * You can define transitions between states that are triggered by events of
- * any kind. The example below registers for click events when the inactive
- * state is entered. In case of a click event the callback is executed and the
- * state machine transitions into the active state:
- *
- *     inactive.on(element.onClick, (event) => active.enter());
- *
- * Also you can automatically trigger callbacks after a timeout. The following
- * snippet calls the callback 1 second after the active state is entered and
- * falls back to the inactive state:
- *
- *     active.onTimeout(const Duration({seconds: 1}), () => inactive.enter());
- *
- * Callbacks often contain code to check for additional constraints and update
- * other objects or UI element before entering a different state. See the
- * tooltip infrastructure in the example directory for a more complete
- * illustration of the functionality provided by this library.
+ * The state machine itself.
  */
 class Machine {
 
@@ -64,7 +29,7 @@ class Machine {
   }
 
   /**
-   * Resets the state machine to its initial state and starts it.
+   * Resets the state machine to its initial state.
    */
   void reset() {
     assert(_initial != null);
@@ -72,7 +37,7 @@ class Machine {
   }
 
   /**
-   * Returns the current state of this machine.
+   * Returns the current state of this machine (for testing only).
    */
   State get current => _current;
 
--- lib/statemachine.dart
+++ lib/statemachine.dart
@@ -1,5 +1,41 @@
 // Copyright (c) 2013, Lukas Renggli <renggli@gmail.com>
 
+/**
+ * To create a new state machine instantiate [Machine]:
+ *
+ *     var machine = new Machine();
+ *
+ * To create states call [Machine#newState] and store them in variables:
+ *
+ *     var inactive = machine.newState();
+ *     var active = machine.newState();
+ *
+ * To reset a state machine to its initial state call [Machine#reset]:
+ *
+ *     machine.reset();
+ *
+ * To enter a specific state call [State#enter].
+ *
+ *     inactive.enter();
+ *
+ * You can define transitions between states that are triggered by events of
+ * any kind using [State#on]. The example below registers for click events
+ * when the inactive state is entered. In case of a click event the callback
+ * is executed and the state machine transitions into the active state:
+ *
+ *     inactive.on(element.onClick, (event) => active.enter());
+ *
+ * Also, you can automatically trigger callbacks after a timeout using
+ * [State#onTimeout]. The following snippet calls the callback 1 second after
+ * the active state is entered and falls back to the inactive state:
+ *
+ *     active.onTimeout(const Duration({seconds: 1}), () => inactive.enter());
+ *
+ * Callbacks often contain code to check for additional constraints and update
+ * other objects or UI element before entering a different state. See the
+ * tooltip infrastructure in the example directory for a more complete
+ * illustration of the functionality provided by this library.
+ */
 library statemachine;
 
 import 'dart:async';
