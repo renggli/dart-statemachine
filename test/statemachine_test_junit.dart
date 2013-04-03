9eab09f5fb4395ed0baeff509e9e1295e68ffb7b
Lukas Renggli <renggli@gmail.com>
2013-04-03 23:42:14 +0200

    - more appropriate naming of junit test

--- test/statemachine_test_junit.dart
+++ test/statemachine_test_junit.dart
@@ -1,6 +1,6 @@
 // Copyright (c) 2013, Lukas Renggli <renggli@gmail.com>
 
-library statemachine_test_jenkins;
+library statemachine_test_junit;
 
 import 'package:junitconfiguration/junitconfiguration.dart';
 import 'statemachine_test.dart' as statemachine_test;
