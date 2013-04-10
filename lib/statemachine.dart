66824d0486fa085e6f595ca44acc9f783cdcd3ae
Lukas Renggli <renggli@gmail.com>
2013-04-10 19:24:26 +0200

    - improved class docs

--- lib/statemachine.dart
+++ lib/statemachine.dart
@@ -5,12 +5,13 @@
  *
  *     var machine = new Machine();
  *
- * To create states call [Machine#newState] and store them in variables:
+ * To create states call [Machine.newState] and store them in variables:
  *
  *     var inactive = machine.newState();
  *     var active = machine.newState();
  *
- * To reset a state machine to its initial state call [Machine#reset]:
+ * To start a state machine or to reset its state to its initial state call
+ * [Machine.reset]:
  *
  *     machine.reset();
  *
@@ -19,14 +20,14 @@
  *     inactive.enter();
  *
  * You can define transitions between states that are triggered by events of
- * any kind using [State#on]. The example below registers for click events
+ * any kind using [State.onStream]. The example below registers for click events
  * when the inactive state is entered. In case of a click event the callback
  * is executed and the state machine transitions into the active state:
  *
- *     inactive.on(element.onClick, (event) => active.enter());
+ *     inactive.onStream(element.onClick, (event) => active.enter());
  *
  * Also, you can automatically trigger callbacks after a timeout using
- * [State#onTimeout]. The following snippet calls the callback 1 second after
+ * [State.onTimeout]. The following snippet calls the callback 1 second after
  * the active state is entered and falls back to the inactive state:
  *
  *     active.onTimeout(const Duration({seconds: 1}), () => inactive.enter());
