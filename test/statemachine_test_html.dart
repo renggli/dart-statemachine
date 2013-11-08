93bdce862e1e7d004e8b498b647390ef2e1a36a8
Lukas Renggli <renggli@gmail.com>
2013-11-08 19:17:52 +0100

    - ready for Dart 1.0 - added html tests

---
+++ test/statemachine_test_html.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2013, Lukas Renggli <renggli@gmail.com>
+
+library statemachine_test_html;
+
+import 'package:unittest/html_enhanced_config.dart';
+import 'statemachine_test.dart' as statemachine_test;
+
+void main() {
+  useHtmlEnhancedConfiguration();
+  statemachine_test.main();
+}
